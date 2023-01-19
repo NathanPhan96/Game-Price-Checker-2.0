e13225a329d5b9b7c74e78941245387128ebd87c
Chitosama <lilchitodude51@gmail.com>
2023-01-19 00:24:18 -0800

    Rewored db for future game graph and new login details

--- db/schema.sql
+++ db/schema.sql
@@ -3,34 +3,59 @@ CREATE DATABASE games_db;
 
 USE games_db;
 
-CREATE TABLE user (
-        userID INT NOT NULL UNIQUE IDENTITY(100,1),
-        email VARCHAR(50) NOT NULL UNIQUE,
-        userName VARCHAR(50)NOT NULL UNIQUE CONSTRAINT check_lowercase_name check (lower(game_name) = game_name),
-        PRIMARY KEY (userID)
-        
-);
+CREATE TABLE IF NOT EXISTS accounts(
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		username VARCHAR(50) NOT NULL UNIQUE,
+		password VARCHAR(255) NOT NULL,
+		email VARCHAR(100) NOT NULL UNIQUE
+) DEFAULT CHARSET=utf8;
 
-CREATE TABLE games(
-		gameId INT NOT NULL UNIQUE default newid() PRIMARY KEY,
-		game_name VARCHAR(70) NOT NULL UNIQUE ALTERNATE KEY CONSTRAINT check_lowercase_name check (lower(game_name) = game_name),
-);
-
-CREATE TABLE gameInfo(
-		gameID INT NOT NULL UNIQUE FOREIGN KEY,
-		gameName FOREIGN KEY,
-		gamePlateform FOREIGN KEY,
-		gamePrice NOT NULL ,
-		url,
-		);
-		
-CREATE TABLE Platforms(
-		id INT NOT NULL UNIQUE AUTO_INCREMNET,
+CREATE TABLE platforms(
+		id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 		console VARCHAR(50) NOT NULL UNIQUE
-)
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE game(
+		id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(75) NOT NULL UNIQUE,
+		platform INT NOT NULL,
+		price DECIMAL(5,2),
+		url VARCHAR(512),
+		account_id INT NOT NULL,
 		
-Create TABLE price_graph (
-		gameName,
-		gamePrice,
+		FOREIGN KEY (account_id)
+			REFERENCES accounts(id),
+			
+		FOREIGN KEY (platform)
+			REFERENCES platforms(id)
+		) DEFAULT CHARSET=utf8;
 		
-		)
+Create TABLE history_price(
+		game_id INT NOT NULL,
+		game_price DECIMAL(5,2),
+		
+		FOREIGN KEY (game_id)
+			REFERENCES game(id)
+		) DEFAULT CHARSET=utf8;
+		
+DROP TRIGGER IF EXISTS history_trigger
+delimiter //
+CREATE TRIGGER history_trigger
+BEFORE UPDATE ON game
+    FOR EACH ROW
+    BEGIN
+        IF OLD.price != NEW.price
+        THEN
+                INSERT INTO history_price
+                    (
+                        game_id,
+                        game_price
+                    )
+                    VALUES
+                    (
+                        NEW.id,
+                        NEW.price
+                    );
+        END IF;
+    END;
+delimiter ;
