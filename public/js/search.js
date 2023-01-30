86fe46d236827196feac8d81df30b04333c4c12c
Hurtorc <anire88@gmail.com>
2023-01-29 22:41:15 -0800

    serach funtionality

--- public/js/search.js
+++ public/js/search.js
@@ -3,21 +3,17 @@
 //if the game is not found, return false
 const { Game } = require("../models");
 
-
-
-
 const searchGame = async (gameName) => {
   const game = await Game.findOne({
     where: {
       name: gameName,
     },
-    });
-    if (game) {
-        return game;
-        } else {
-            return false;
-        }
-    };
-
+  });
+  if (game) {
+    return game;
+  } else {
+    return false;
+  }
+};
 
 module.exports = searchGame;
